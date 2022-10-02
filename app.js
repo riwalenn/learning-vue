eb71dd1df12472d66dc849e5ba8968ac4c3297ea
RIWALENN BAS <riwalenn@gmail.com>
2022-10-02 16:48:05 +0200

    conditional rendering

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         }
     },
     methods: {
-      changeTitle(title) {
-          // this.title = "Something else"
-          this.title = title
-      }
-    },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
 });
 app.mount('#app');
