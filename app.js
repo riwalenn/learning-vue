09075fb34c4d4fd1fc49787ae63718db82c1f0a4
RIWALENN BAS <riwalenn@gmail.com>
2022-10-02 16:28:58 +0200

    lesson 8

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    }
+});
+app.mount('#app');
