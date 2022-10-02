488bab31f14f0b0522b0cbe7a561be18b05405a9
RIWALENN BAS <riwalenn@gmail.com>
2022-10-02 17:53:01 +0200

    attribute binding

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.riwalennbas.com',
             showBooks: true,
             books: [
                 {
-                    title: 'The Final Empire',
-                    author: 'Brandon Sanderson',
+                    title: ' Hooked : comment créer un produit ou un service qui ancre des habitudes',
+                    author: 'Nir Eyal, Pascale-Marie Deschamps',
+                    img: 'https://m.media-amazon.com/images/P/B07G5MW81B.01._SCLZZZZZZZ_SX500_.jpg',
                 },
                 {
-                    title: 'The way of kings',
-                    author: 'Patrick Rothfuss',
+                    title: 'Proprement Codeur. Code de conduite pour développeur professionnel',
+                    author: 'Robert C. Martin',
+                    img: 'https://m.media-amazon.com/images/I/41cM+1mcKoL._SX404_BO1,204,203,200_.jpg',
                 },
                 {
-                    title: 'name of the wind',
-                    author: 'Brandon Sanderson',
+                    title: 'Building Microservices: Designing Fine-Grained Systems',
+                    author: 'Sam Newman',
+                    img: 'https://m.media-amazon.com/images/I/519hB5niytL._SX379_BO1,204,203,200_.jpg',
                 },
             ]
         }
